e5a146714d8ebde1b5b5985a2c066330596e0938
Daigo Naruse <s1711459@s.tsukuba.ac.jp>
2020-06-12 11:38:09 +0900

    insert_koyo_frag.pyを作成

---
+++ script/get_relation_words.py
@@ -0,0 +1,78 @@
+"""
+教師データのツイート内に含まれる各単語と
+'桜'の関連度を計算する
+"""
+
+from s_lib import setup_mongo
+import sys, math, collections
+
+
+def calc_pmi(sw, w, s, N):
+  pmi = math.log2(((sw + 1) * N) / (w * s))
+  return(pmi)
+
+
+def calc_soa(sw, ns_w, w, s, ns, N):
+    #  soa = math.log2(((sw + 1) * ns)/((ns_w + 1) * s))
+    soa = calc_pmi(sw, w, s, N) - calc_pmi(ns_w, w, ns, N)
+    return(soa)
+
+
+def main():
+    args = sys.argv
+
+    db = setup_mongo('2014_sakura_twi_1208')
+    pname = 'hk'
+
+    col = db['season_' + pname]
+    s_twis = list(col.find({'sakura_twi': 1}))
+    ns_twis = list(col.find({'sakura_twi': 0}))
+
+    words = []
+    ns_words = {}
+    for s_twi in s_twis:
+        words.extend(s_twi['morpho_text'].split(' '))
+    uwords = list(set(words))
+    for ns_twi in ns_twis:
+        morpho = ns_twi['morpho_text'].split(' ')
+        for w in uwords:
+            if w in morpho:
+                ns_words[w] = ns_words[w] + 1 if w in ns_words else 1
+
+    sw_count = collections.Counter(words)
+    s = len(s_twis)
+    ns = len(ns_twis)
+    N = s + ns
+
+    pmi_of_word = {}
+    for w, c in sw_count.items():
+        if w in ['桜', 'さくら', 'サクラ']:
+            continue
+        sw = c
+        ns_w = ns_words[w] if w in ns_words else 0
+        wc = sw + ns_w
+        if not any([wc < 1, s < 1]):
+            pmi_of_word[w] = calc_pmi(sw, wc, s, N)
+    sorted_pmi = sorted(pmi_of_word.items(), key=lambda x:x[1], reverse=True)
+
+    with open('/now24/a.saito/tmp/' + pname + '_pmi.txt', 'w') as f:
+        for w, pmi in sorted_pmi:
+            f.write(f'{w}, {pmi}\n')
+
+    soa_of_word = {}
+    for w, c in sw_count.items():
+        if w in ['桜', 'さくら', 'サクラ']:
+            continue
+        sw = c
+        ns_w = ns_words[w] if w in ns_words else 0
+        wc = sw + ns_w
+        if not s < 1:
+            soa_of_word[w] = calc_soa(sw, ns_w, wc, s, ns, N)
+    sorted_soa = sorted(soa_of_word.items(), key=lambda x:x[1], reverse=True)
+    with open('/now24/a.saito/tmp/' + pname + '_soa.txt', 'w') as f:
+        for w, soa in sorted_soa:
+            f.write(f'{w}, {soa}\n')
+
+
+main()
+
---
+++ script/insert_koyo_flag.py
@@ -0,0 +1,20 @@
+"""
+既存の桜DBに、紅葉キーワードが含まれているか判定するflagとなるフィールドを追加するスクリプト
+"""
+from pymongo import MongoClient
+from s_lib import setup_mongo, setup_mecab
+
+keywords = ["紅葉", "モミジ", "もみじ", "かえで", "カエデ", "楓", "イチョウ", "銀杏"]
+
+db = setup_mongo('2014_sakura_twi_1208')
+pname_list = ['hk'] # , 'tk', 'is'
+
+for pname in pname_list:
+    col = db['season_' + pname]
+
+    for post in col.find():
+        mors = post['morpho_text'].split(" ")
+        if len(set(keywords) & set(mors)) > 0:
+            col.update_one({'_id':post['_id']}, {'$set':{"koyo":1}})
+        else:
+            col.update_one({'_id':post['_id']}, {'$set':{"koyo":0}})
