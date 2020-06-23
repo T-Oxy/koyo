70f2a1307dc79ea388a46ef8590abe2c7d4f7d19
Daigo Naruse <s1711459@s.tsukuba.ac.jp>
2020-06-24 00:08:39 +0900

    2015のフラグinsertプログラムに無駄があったので削除

--- script/insert_ichof_2015.py
+++ script/insert_ichof_2015.py
@@ -7,20 +7,15 @@ from s_lib import setup_mongo, setup_mecab
 keywords = ["いちょう", "イチョウ", "銀杏"]
 
 db = setup_mongo('2015_tk_twi')
-pname_list = ['tk'] # 'hk', 'is']
 
-for pname in pname_list:
-    for month in range(2, 13):
-        month = str(month).zfill(2)
+for month in range(2, 13):
+    month = str(month).zfill(2)
 
-        col = db["2015-"+month]
+    col = db["2015-"+month]
 
-        for post in col.find():
-            mors = post['morpho_text'].split(" ")
-            if len(set(keywords) & set(mors)) > 0:
-                col.update_one({'_id':post['_id']}, {'$set':{"icho":1}})
-            else:
-                col.update_one({'_id':post['_id']}, {'$set':{"icho":0}})
-
-
-                
+    for post in col.find():
+        mors = post['morpho_text'].split(" ")
+        if len(set(keywords) & set(mors)) > 0:
+            col.update_one({'_id':post['_id']}, {'$set':{"icho":1}})
+        else:
+            col.update_one({'_id':post['_id']}, {'$set':{"icho":0}})
