79d4e13eba594bec9c83a61a1ec6446b04857353
Daigo Naruse <s1711459@s.tsukuba.ac.jp>
2020-07-29 21:15:17 +0900

    cut_month.py修正

--- script/cut_month.py
+++ script/cut_month.py
@@ -10,13 +10,13 @@ def cut_month(fromdb, todb):
     prefs = ["tk", "hk", "is"]
     mons = [10, 11, 12]
 
-    for pref in tqdm(prefs):
+    for pref in prefs:
         tocol = todb[pref]
-        for mon in mons:
+        for mon in tqdm(mons):
             fromcol = fromdb["2014_" + str(mon) + "_" + pref]
             docs = fromcol.find()
             for i in docs:
-                tocol.insert_one(i)
+                tocol.insert_one(docs)
 
 def main():
     fromdb = setup_mongo('2014_twi')
